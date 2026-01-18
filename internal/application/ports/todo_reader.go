059d04e1ef8ed4805e01de2c02cc8857041dda4d
Carlos Coronado <coronadorodrigo31@gmail.com>
2026-01-17 23:02:15 -0400

    feat: add command to handle todo update to mark as done

--- internal/application/ports/todo_reader.go
+++ internal/application/ports/todo_reader.go
@@ -4,5 +4,5 @@ import "github.com/crdev13/clean-architecture-cqrs-todo-go/internal/domain"
 
 type TodoReader interface {
 	ListByUser(userID string) ([]domain.Todo, error)
-	GetById(todoID string) (*domain.Todo, error)
+	GetByID(todoID string) (*domain.Todo, error)
 }
